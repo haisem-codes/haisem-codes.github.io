99e89a1a73c50222b80c4e1cbb1b9d3fd0321e75
Haisem Naeem <haisem.work@gmail.com>
2026-03-29 12:37:17 +0500

    feat: add teal favicon, web manifest, and fix GitHub Pages
    - Teal "H" favicon in all sizes (16, 32, 180, 192, 512px)
    - SVG favicon for sharp rendering at any size
    - Apple touch icon for iOS
    - Web manifest for PWA support
    - Favicon links in layout head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,6 +58,10 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
         <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
